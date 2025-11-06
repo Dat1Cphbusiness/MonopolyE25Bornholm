5e1fc085e8daccad885ae095a2553618f94d157b
Jon Bertelsen <jobe@cphbusiness.dk>
2025-11-06 09:34:59 +0100

    refactor Game class

--- src/entities/Game.java
+++ src/entities/Game.java
@@ -1,13 +1,10 @@
 package entities;
 
-import entities.spaces.Property;
-import entities.spaces.Tax;
+import UI.GameUI;
+import factories.GameFactory;
 import utils.FileIO;
 import utils.TextUI;
-
 import java.util.ArrayList;
-import java.util.Collections;
-
 
 public class Game {
 
@@ -24,6 +21,8 @@ public class Game {
     private Dice dice;
     TextUI ui = new TextUI();
     FileIO io = new FileIO();
+    GameFactory gameFactory = new GameFactory();
+    GameUI gameUI = new GameUI();
 
     public Game(String name, int maxPlayers) {
         this.name = name;
@@ -37,110 +36,12 @@ public class Game {
     }
 
     public void setup() {
-
-        initializePlayers();
-        initializeBoard();
-        initializeCards();
-        initializeDeeds();
-
-        displayPlayers();
-        displayBoard();
-
-    }
-
-    private void initializeDeeds() {
-        ArrayList<String> deedData = io.readData("data/deedData.csv");
-
-        if (!deedData.isEmpty()) {
-            for (String s : deedData) {
-                if (s.startsWith("Number"))
-                    continue;
-                String[] values = s.split(",");//  "2, Property, rent,1,2,3
-
-                int number = ui.getIntOrZero(values[0].trim());
-                String type = values[1].trim();
-                int basicRent = 0, rentTwo = 0, rentThree = 0, rentFour = 0, rentOne = 0, rentHotel = 0, housePrice = 0, hotelPrice = 0, mortgage = 0;
-                if (values.length > 2) {
-                    basicRent = ui.getIntOrZero(values[2].trim());
-                    rentTwo = ui.getIntOrZero(values[4].trim());
-                    rentThree = ui.getIntOrZero(values[5].trim());
-                    rentFour = ui.getIntOrZero(values[6].trim());
-                    mortgage = ui.getIntOrZero(values[10].trim());
-                }
-
-                switch (type) {
-                    case "Property":
-
-                        if (values.length > 3) {
-                            rentOne = ui.getIntOrZero(values[3].trim());
-                            rentHotel = ui.getIntOrZero(values[7].trim());
-                            housePrice = ui.getIntOrZero(values[8].trim());
-                            hotelPrice = ui.getIntOrZero(values[9].trim());
-                            deeds.add(new Deed(number, type, basicRent, rentOne, rentTwo, rentThree, rentFour, rentHotel, housePrice, hotelPrice, mortgage));
-
-                        }
-                        break;
-
-                    case "Shipping Company":
-                        if (values.length > 1) {
-                            deeds.add(new Deed(number, type, basicRent, rentTwo, rentThree, rentFour, mortgage));
-                        }
-                        break;
-
-                    case "Brewery":
-                        deeds.add(new Deed(mortgage));
-                        break;
-                }
-            }
-        }
-    }
-
-
-    private void initializeCards() {
-        // TODO: Skal kodes
-        int amount = 0;
-        ArrayList<String> chanceCardsData = io.readData("data/chanceCard.csv");
-        if (!chanceCardsData.isEmpty()) {
-            for (String s : chanceCardsData) {
-                String[] values = s.split(",");//
-                String type = values[0];
-
-                if (!type.equals("MoveJail")) {
-                    amount = Integer.parseInt(values[1].trim());
-
-                } else
-                    if (values[1].equals("To jail")) {
-                        amount = 0;
-                    }
-
-                String instruction = values[2];
-                chanceCards.add(new ChanceCard(type, amount, instruction));
-
-            }
-        }
-        Collections.shuffle(chanceCards);
-    }
-
-    private void showGameMenu() {
-        ArrayList<String> menuItems = new ArrayList<>();
-        ArrayList<String> result = new ArrayList<>();
-        menuItems.add("Rul");
-        menuItems.add("Se spillerinfo");
-        menuItems.add("Se brættet");
-        menuItems.add("Afslut spil");
-        result = ui.promptChoice(menuItems, 1, "Hvad vil du nu?");
-        String choice = result.get(0);
-        ui.displayMsg("Du har valgt: " + choice);
-
-        switch (choice) {
-            case "Se spillerinfo":
-                displayPlayers();
-                break;
-            default:
-                break;
-        }
-
-
+        gameFactory.initializePlayers(io, ui, START_CASH, maxPlayers, players);
+        gameFactory.initializeBoard(io, ui, board);
+        gameFactory.initializeCards(io, chanceCards);
+        gameFactory.initializeDeeds(io, ui, deeds);
+        gameUI.displayPlayers(ui, players);
+        gameUI.displayBoard(ui, board);
     }
 
     public void gameLoop() {
@@ -150,16 +51,8 @@ public class Game {
         int counter = 0;
 
         while (continueGame) {
-
-            // get currentPlayer
             currentPlayer = players.get(currentPlayerIndex);
-
-            // roll
             int diceRoll = dice.roll();
-
-            //diceRoll = 4;  // tax
-
-            // Move and check for double eyes
 
             if (dice.pair()) {
                 currentPlayer.increaseStreak();
@@ -188,8 +81,7 @@ public class Game {
                 ui.displayMsg("Vi har ikke implementeret denne felttype endnu");
             }
 
-
-            showGameMenu();
+            gameUI.showGameMenu(ui, players);
 
             // TODO: Remove this counter stuff below when we're done and want to manually play
             counter++;
@@ -208,74 +100,6 @@ public class Game {
         }
     }
 
-    // Read players from file or register player from console
-    private void initializePlayers() {
-        ArrayList<String> data = io.readData("data/playerData.csv");
-        if (!data.isEmpty()) {
-            for (String s : data) {
-                String[] values = s.split(",");//  "tess, 0
-                String name = values[0];
-                int cash = Integer.parseInt(values[1].trim());
-                players.add(new Player(name, cash));
-            }
-        } else {
-            registerPlayers();
-        }
-        Collections.shuffle(players);
-    }
-
-    private void initializeBoard() {
-        ArrayList<String> boardData = io.readData("data/boardData.csv");
-
-        if (!boardData.isEmpty()) {
-            for (String s : boardData) {
-                String[] values = s.split(",");  //  "2,Property,BabyBlue,Rødovrevej,1200,,,Jon"
-                int id = Integer.parseInt(values[0].trim());
-                String type = values[1].trim();
-
-                switch (type) {
-                    case "Property":
-                        String color = values[2].trim();
-                        String name = values[3].trim();
-                        int price = Integer.parseInt(values[4].trim());
-                        Space property = new Property(id, name, type, color, price);
-                        board.addSpace(property);
-                        break;
-                    case "Tax":
-                        name = values[3].trim();
-                        String taxText = values[5].trim();
-                        Space tax = new Tax(id, name, type, taxText);
-                        board.addSpace(tax);
-                        break;
-                    default:
-                        ui.displayMsg("type: " + type + " not implemented yet");
-                }
-            }
-        }
-
-    }
-
-
-    public void registerPlayers() {
-        while (this.players.size() < this.maxPlayers) {
-            String playerName = ui.promptText("Tast spiller navn");
-            players.add(new Player(name, START_CASH));
-        }
-    }
-
-    public void displayPlayers() {
-        ui.displayMsg("***** Players: ********");
-        for (Player player : players) {
-            ui.displayMsg(player.toString());
-        }
-    }
-
-    public void displayBoard() {
-        ui.displayMsg("***** Board: ********");
-        for (Space s : board.getSpaces()) {
-            ui.displayMsg("Space: " + s.toString());
-        }
-    }
 
     public void endSession() {
         ArrayList<String> playerData = new ArrayList<>();
